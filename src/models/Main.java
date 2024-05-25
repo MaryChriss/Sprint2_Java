2f8c262215ce4fbc5fb78b8c7d11d0e4e79815fe
Mariana <marychris.crf@gmail.com>
2024-05-25 17:49:15 -0300

    Iniciando o Main

---
+++ src/models/Main.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    private static final List<Cliente> clientes = new ArrayList<>();
+    private static final List<OficinaMecanica> oficinas = new ArrayList<>();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+
+}
